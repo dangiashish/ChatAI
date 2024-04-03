d8bbddcbf80f26b9c37341e2c1b32af3570fd3af
Ashish <ashishdangi96@gmail.com>
2024-04-03 14:45:15 +0530

    Initial commit

--- app/src/main/java/com/codebyashish/chatai/networking/RetrofitClient.kt
+++ app/src/main/java/com/codebyashish/chatai/networking/RetrofitClient.kt
@@ -13,38 +13,6 @@ class RetrofitClient {
      val APPLICATION_URL_ENCODED = "application/json"
      val KEY_AUTHORIZATION = "Authorization"
 
-    fun getTokenClient() : Retrofit {
-
-        val builder : OkHttpClient.Builder = OkHttpClient.Builder()
-        builder.readTimeout(15000, TimeUnit.SECONDS)
-        val loggingInterceptor  = HttpLoggingInterceptor()
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-
-        val header = Interceptor { chain: Interceptor.Chain ->
-            val original = chain.request()
-           val accessToken = "Bearer ${BuildConfig.CHAT_GPT_KEY}"
-//           val accessToken = "Bearer "
-
-            val request = original.newBuilder()
-                .header(KEY_CONTENT_TYPE, APPLICATION_URL_ENCODED)
-                .header(KEY_AUTHORIZATION, accessToken)
-                .method(original.method, original.body)
-                .build()
-            chain.proceed(request)
-        }
-
-        builder.addInterceptor(header).addInterceptor(loggingInterceptor)
-
-        val client = builder.build()
-
-        return Retrofit.Builder()
-            .baseUrl("https://api.openai.com/v1/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .client(client)
-            .build()
-    }
-
     fun interceptor(apiKey : String) : Interceptor {
         return Interceptor { chain: Interceptor.Chain ->
             val original = chain.request()
